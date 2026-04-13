f4247bcdc7d506eed11583847bada7fd39cfab24
Nikolaos Bellias <nikolaos.bellias@gmail.com>
2026-04-13 17:03:15 +0300

    Added Third page with MyModal demos

---
+++ MySolution.MyBlazorWASM/Components/MyModal.razor.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Components;
+
+namespace MySolution.MyBlazorWASM.Components;
+
+public partial class MyModal : ComponentBase
+{
+}
---
+++ MySolution.MyBlazorWASM/Components/MyModal.razor.js
@@ -0,0 +1,5 @@
+export class MyModal {
+  
+}
+
+window.MyModal = MyModal;
